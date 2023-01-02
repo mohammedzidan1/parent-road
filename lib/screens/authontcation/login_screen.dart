bc51e88274e1be33a88fdb036df78e07e5ff7872
Mohamed Zydan <mohmmedzidane@gmail.com>
2023-01-02 13:19:31 +0200

    scroll

--- lib/screens/authontcation/login_screen.dart
+++ lib/screens/authontcation/login_screen.dart
@@ -40,115 +40,117 @@ class LoginScreen extends StatelessWidget {
         ),
         margin: const EdgeInsets.only(top: 10),
         padding: const EdgeInsets.only(left: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomText(
-              text: "Welcome Back",
-              color: ColorsApp.mainTextColor,
-              fontSise: 32,
-              fontWeight: FontWeight.bold,
-            ),
-            CustomText(
-              text: "Enter Your Password & Email",
-              color: ColorsApp.mainTextColor,
-              fontSise: 24,
-              fontWeight: FontWeight.bold,
-            ),
-            const SizedBox(
-              height: 150,
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 5),
-
-
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-
-                    alignment: Alignment.topLeft,
-
-                    child: CustomText(
-                      text: "Email",
-                      color: ColorsApp.primaryColor,
-                      fontSise: 24,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const CustomTextField(
-                    color: ColorsApp.white,
-                    keyBordType: TextInputType.emailAddress,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: CustomText(
-                      text: "Password",
-                      color: ColorsApp.primaryColor,
-                      fontSise: 24,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const CustomTextField(
-                    text: "password",
-                    color: ColorsApp.white,
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomText(
+                text: "Welcome Back",
+                color: ColorsApp.mainTextColor,
+                fontSise: 32,
+                fontWeight: FontWeight.bold,
               ),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * .17,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: Column(
-                children: [
-                  const CustomButton(
-                    width: 230,
-                    height: 51,
-                    text: "LOGIN",
+              CustomText(
+                text: "Enter Your Password & Email",
+                color: ColorsApp.mainTextColor,
+                fontSise: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              const SizedBox(
+                height: 150,
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 5),
 
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      MaterialPageRoute(
-                          builder: (context) => const SignUpScreen());
-                    },
-                    child: InkWell(
-                      onTap: () {},
+
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Align(
+
+                      alignment: Alignment.topLeft,
+
                       child: CustomText(
-                        text: "Forget the password ! ",
+                        text: "Email",
                         color: ColorsApp.primaryColor,
-                        fontWeight: FontWeight.bold,
-                        fontSise: 15,
+                        fontSise: 24,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  CustomText(
-                    text: "Or Create a New account! ",
-                    color: ColorsApp.primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSise: 15,
-                  ),
-                ],
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const CustomTextField(
+                      color: ColorsApp.white,
+                      keyBordType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: CustomText(
+                        text: "Password",
+                        color: ColorsApp.primaryColor,
+                        fontSise: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const CustomTextField(
+                      text: "password",
+                      color: ColorsApp.white,
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .17,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const CustomButton(
+                      width: 230,
+                      height: 51,
+                      text: "LOGIN",
+
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        MaterialPageRoute(
+                            builder: (context) => const SignUpScreen());
+                      },
+                      child: InkWell(
+                        onTap: () {},
+                        child: CustomText(
+                          text: "Forget the password ! ",
+                          color: ColorsApp.primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSise: 15,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    CustomText(
+                      text: "Or Create a New account! ",
+                      color: ColorsApp.primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSise: 15,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
